0a598ec81cf649858abcbbdf5df4e04bbee2e33c
GreatInnik <innikhafizov@gmail.com>
2024-10-29 15:20:28 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+finished_homework = 12
+lost_time = 1.5
+name_of_kurs = "Python"
+time_for_one_work = lost_time / finished_homework
+print("Курс:", name_of_kurs, ",всего задач:", finished_homework, ",затрачено часов:", lost_time, ",среднее время выполнения:", time_for_one_work, "часа")
