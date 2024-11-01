e2f416b0f9d359548efbdc2d05ad3885997da323
GreatInnik <innikhafizov@gmail.com>
2024-11-01 15:07:52 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, True, False, 'Feel'
+print(immutable_var)
+#immutable_var[0] = 1
+#Кортеж - неизменяющийся список. Он фиксированный
+mutable_list = ([1, 2, 3], 4)
+mutable_list[0][0] = 5
+print(mutable_list)
