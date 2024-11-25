06195da3fcf70b912dff61f472d6cefbf9b1722b
GreatInnik <innikhafizov@gmail.com>
2024-11-25 16:59:28 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,16 +1,19 @@
 def get_multiplied_digits(number):
     if number == 0:
-        return number
+        return 1  # Изменяем на 1, чтобы не влиять на произведение
     str_number = str(number)
     first = int(str_number[0])
 
-    if first == 0:
-        return get_multiplied_digits(int(str_number[1:]))
+    # Игнорируем нули в конце числа
+    while str_number and str_number[-1] == '0':
+        str_number = str_number[:-1]
+
+    if not str_number:  # Если все цифры были нулями
+        return 1
 
     if len(str_number) == 1:
         return first
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
-
-print(get_multiplied_digits(1002))
+print(get_multiplied_digits(100420))
