eb544d5bd391dd8e6b64ac3dd20a2e8787c8e7fd
GreatInnik <innikhafizov@gmail.com>
2024-11-25 16:35:24 +0300

    Update module_2_1.py

--- module_2_1.py
+++ module_2_1.py
@@ -1,13 +1,9 @@
-num1 = int(input('Введите первое число'))
-num2 = int(input('Введите второе число'))
-num3 = int(input('Введите третье число'))
-if num1 == num2 and num2 == num3:
+first = int(input('Введите первое число'))
+second = int(input('Введите второе число'))
+third = int(input('Введите третье число'))
+if first == second and second == third:
     print('3')
-elif num1 == num2:
-    print('2')
-elif num2 == num3:
-    print('2')
-elif num1 == num3:
+elif first == second or second == third or first == third:
     print('2')
 else:
     print('0')
