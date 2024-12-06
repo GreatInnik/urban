dfeb505cb4afcc7752543f8f22d22dd438a9fee2
GreatInnik <innikhafizov@gmail.com>
2024-12-06 16:21:53 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,13 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            return "Такого этажа не существует"
+        else:
+            return f"Вы на {new_floor} этаже"
+
+h1 = House("ЖК Аврора,", 30)
+
+print(h1.name, h1.number_of_floors, "- этажное здание,", h1.go_to(3))
